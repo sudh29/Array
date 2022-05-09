2d68033da9f7cbd9fc332513700bf0b6bbbf0720
Sudhanshu Chaudhary <73557822+sudh29@users.noreply.github.com>
2022-05-09 15:38:54 +0530

    Update 6_Cyclically_rotate_an_array_by_one.py

--- 6_Cyclically_rotate_an_array_by_one.py
+++ 6_Cyclically_rotate_an_array_by_one.py
@@ -1,7 +1,9 @@
 def rotate( arr, n):
     m=len(arr)
     rem=n%(m+1)
-    #print(m,n,rem)
-    for i in range(rem-1):
+    
+    '''for i in range(rem-1):
         temp=arr.pop(0)
-        arr.append(temp)
+        arr.append(temp)'''
+    for i in range(rem-1,0,-1):
+        arr[i],arr[i-1]=arr[i-1],arr[i]
