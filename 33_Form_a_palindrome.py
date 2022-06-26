4f814d6e0f79384ecda7c41c3c0fadbc24fa5bbd
Sudhanshu Chaudhary <73557822+sudh29@users.noreply.github.com>
2022-06-26 19:49:24 +0530

    Update 33_Form_a_palindrome.py

--- 33_Form_a_palindrome.py
+++ 33_Form_a_palindrome.py
@@ -16,3 +16,24 @@ def findMinInsertionsDP(str1, n):
 class Solution:
     def countMin(self, Str):
         return findMinInsertionsDP(Str, len(Str))
+    
+    
+def merge(ar):
+    # Write your code here
+    n=len(ar)
+    s=0
+    e=n-1
+    res=0
+    while s<=e:
+        if ar[s]==ar[e]:
+            s+=1
+            e-=1
+        elif ar[s]<ar[e]:
+            s+=1
+            ar[s]=ar[s]+ar[s-1]
+            res+=1
+        else:
+            e-=1
+            ar[e]=ar[e]=ar[e+1]
+            res+=1
+    return res;
