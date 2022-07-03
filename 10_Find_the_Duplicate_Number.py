3fcdbb057ec2f26e7d68472106502e80ded9c6b7
Sudhanshu Chaudhary <73557822+sudh29@users.noreply.github.com>
2022-07-03 21:37:40 +0530

    Update 10_Find_the_Duplicate_Number.py

--- 10_Find_the_Duplicate_Number.py
+++ 10_Find_the_Duplicate_Number.py
@@ -5,13 +5,22 @@ class Solution:
         sum_nums_set=sum(nums_set)
         return sum_nums-sum_nums_set'''
     
-        tem={}
-        for i in nums:
-            if i not in tem:
-                tem[i]=1
-            else:
-                tem[i]+=1
-        for k,v in tem.items():
-            if v>1:
-                return k
-        return
+#         tem={}
+#         for i in nums:
+#             if i not in tem:
+#                 tem[i]=1
+#             else:
+#                 tem[i]+=1
+#         for k,v in tem.items():
+#             if v>1:
+#                 return k
+#         return
+    
+        n=len(nums)
+        indx=0
+        for i in range(n):
+            indx=abs(nums[i])-1
+            if (nums[indx]<0):
+                return abs(nums[i])
+            nums[indx]=-nums[indx]
+        return -1
