69899c6c974c89bb19999fbd8bb3a79e367233d0
Sudhanshu Chaudhary <73557822+sudh29@users.noreply.github.com>
2022-07-13 21:44:25 +0530

    Update 0_Reverse_the_array.py

--- 0_Reverse_the_array.py
+++ 0_Reverse_the_array.py
@@ -1,7 +1,10 @@
-#code
 t=int(input())
 #print(t)
 for i in range(t):
     n=int(input())
     arr=input().split()
-    print(" ".join(arr[::-1]))
+    # print(arr)
+    for i in range(len(arr)//2):
+        arr[i],arr[n-1-i]=arr[n-1-i],arr[i]
+    print(" ".join(arr))    
+    # print(" ".join(arr[::-1]))
