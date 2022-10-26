1b0c275220c6b8a315b06a0c7672e037a404bcb4
Sudhanshu Chaudhary <73557822+sudh29@users.noreply.github.com>
2022-10-27 00:36:59 +0530

    Update 4_Move_all_negative_elements_to_end.py

--- 4_Move_all_negative_elements_to_end.py
+++ 4_Move_all_negative_elements_to_end.py
@@ -1,9 +1,27 @@
 class Solution:
-    def segregateElements(self, arr, n):
+        def segregateElements(self, arr, n):
         # Your code goes here
-        temp1=[i for i in arr if i<0]
-        temp2=[i for i in arr if i>=0]
-        temp=(temp2+temp1)
+        # temp1=[i for i in arr if i<0]
+        # temp2=[i for i in arr if i>=0]
+        # temp=(temp2+temp1)
+        # for i in range(n):
+        #     arr[i]=temp[i]
+        # return arr
+        
         for i in range(n):
-            arr[i]=temp[i]
-        return arr
+            if arr[i]>=0:
+                pos=i
+                break
+        for i in range(n):
+            if arr[i]<0:
+                neg=i
+                break
+        while(pos<n and neg<n):
+            if pos<neg:
+                pos+=1
+            else:
+                arr[pos],arr[neg]=arr[neg],arr[pos]
+                pos+=1
+                neg+=1
+        
+        print(arr)
