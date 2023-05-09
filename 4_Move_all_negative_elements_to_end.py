8b5344d0a95a1810b4847ba2e3e2024fadc84d13
Sudhanshu Chaudhary <73557822+sudh29@users.noreply.github.com>
2023-05-09 23:06:19 +0530

    Update 4_Move_all_negative_elements_to_end.py

--- 4_Move_all_negative_elements_to_end.py
+++ 4_Move_all_negative_elements_to_end.py
@@ -25,3 +25,18 @@ class Solution:
                 neg+=1
         
         print(arr)
+
+        
+# Two pointer
+def move_negatives(arr):
+    left = 0
+    right = len(arr) - 1
+    while left < right:
+        if arr[left] < 0:
+            left += 1
+        elif arr[right] >= 0:
+            right -= 1
+        else:
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
+            right -= 1
