41e73d7155e62d9ffc84233ccf3ed48c94313360
Sudhanshu Chaudhary <73557822+sudh29@users.noreply.github.com>
2023-05-09 11:46:32 +0530

    Update 0_Reverse_the_array.py

--- 0_Reverse_the_array.py
+++ 0_Reverse_the_array.py
@@ -8,3 +8,15 @@ for i in range(t):
         arr[i],arr[n-1-i]=arr[n-1-i],arr[i]
     print(" ".join(arr))    
     # print(" ".join(arr[::-1]))
+
+    
+    
+# Two pointer approach
+
+def reverse_array(arr):
+    start = 0
+    end = len(arr) - 1
+    while start < end:
+        arr[start], arr[end] = arr[end], arr[start]
+        start += 1
+        end -= 1
