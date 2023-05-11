34774127f7b0e3cf493fccbb2037da8cecd949e0
Sudhanshu Chaudhary <73557822+sudh29@users.noreply.github.com>
2023-05-11 19:54:54 +0530

    Update 7_Kadanes_Algorithm.py

--- 7_Kadanes_Algorithm.py
+++ 7_Kadanes_Algorithm.py
@@ -10,3 +10,16 @@ class Solution:
             max_end_her=max(i,max_end_her+i)
             max_so_far=max(max_so_far,max_end_her)
         return max_so_far
+
+    
+    
+    
+def max_subarray_sum(arr):
+    max_ending_here = max_so_far = arr[0]
+    for i in range(1, len(arr)):
+        max_ending_here = max(arr[i], max_ending_here + arr[i])
+        max_so_far = max(max_so_far, max_ending_here)
+    return max_so_far
+
+arr = [-2, -3, 4, -1, -2, 1, 5, -3]
+print(max_subarray_sum(arr))
