9b976ebf788fb2fdac223f45f10d0f3f641949f1
Sudhanshu Chaudhary <73557822+sudh29@users.noreply.github.com>
2024-05-19 22:35:26 +0530

    Update 18_find_common_elements_In_3_sorted_arrays.py

--- 18_find_common_elements_In_3_sorted_arrays.py
+++ 18_find_common_elements_In_3_sorted_arrays.py
@@ -1,31 +1,39 @@
+
 class Solution:
     def commonElements (self,A, B, C, n1, n2, n3):
-        # your code here
+        
+        # Extra space
         temp={}
         A=list(set(A))
         B=list(set(B))
         C=list(set(C))
         for i in A:
-            if i in temp:
-                temp[i]+=1
-            else:
-                temp[i]=1
+            temp[i] = temp.get(i,0)+1
         for i in B:
-            if i in temp:
-                temp[i]+=1
-            else:
-                temp[i]=1
+            temp[i] = temp.get(i,0)+1
         for i in C:
-            if i in temp:
-                temp[i]+=1
-            else:
-                temp[i]=1
+            temp[i] = temp.get(i,0)+1
         res=[]
         temp1=temp
         val=sorted(temp)
-        #print(val)
-        #print(temp1)
         for i in val:
             if temp1[i]==3:
                 res.append(i)
         return res
+        
+        # # 3 Pointer
+        # i, j, k = 0, 0, 0
+        # common_elements = set()
+        # while i < n1 and j < n2 and k < n3:
+        #     if A[i] == B[j] == C[k]:
+        #         common_elements.add(A[i])
+        #         i += 1
+        #         j += 1
+        #         k += 1
+        #     elif A[i] < B[j]:
+        #         i += 1
+        #     elif B[j] < C[k]:
+        #         j += 1
+        #     else:
+        #         k += 1
+        # return sorted(list(common_elements))
