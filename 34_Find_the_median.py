976bff5d9751ff1a22f4e1a24d79c06b3955bd3a
Sudhanshu Chaudhary <73557822+sudh29@users.noreply.github.com>
2024-05-22 21:58:57 +0530

    Update 34_Find_the_median.py

--- 34_Find_the_median.py
+++ 34_Find_the_median.py
@@ -1,10 +1,9 @@
 class Solution:
 	def find_median(self, v):
-		# Code here
 		v.sort()
-		#print(v)
-		n=len(v)
-		if n%2==0:
-		    return (v[n//2]+v[(n//2)-1])//2
-	    else:
-	        return v[n//2]
+		n = len(v)
+		if n % 2 == 0:
+		    return (v[n // 2] + v[(n // 2) - 1]) // 2
+		else:
+		    return v[n // 2]
+
