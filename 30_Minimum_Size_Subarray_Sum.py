878c700209ef05ce403dc65fb66c72769d539ea9
Sudhanshu Chaudhary <73557822+sudh29@users.noreply.github.com>
2024-06-03 18:11:40 +0530

    Update 30_Minimum_Size_Subarray_Sum.py

--- 30_Minimum_Size_Subarray_Sum.py
+++ 30_Minimum_Size_Subarray_Sum.py
@@ -1,7 +1,7 @@
 import sys
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        n=len(nums)
+        '''n=len(nums)
         res=sys.maxsize
         i=0
         j=1
@@ -17,5 +17,20 @@ class Solution:
                 j+=1
                 if j<n:
                     temp+=nums[j]
-        return res if res<sys.maxsize else 0
+        return res if res<sys.maxsize else 0'''
+
+        n = len(nums)
+        left = 0
+        total = 0
+        min_length = float('inf')
+
+        for right in range(n):
+            total += nums[right]
+
+            while total >= target:
+                min_length = min(min_length, right - left + 1)
+                total -= nums[left]
+                left += 1
+
+        return min_length if min_length != float('inf') else 0
         
