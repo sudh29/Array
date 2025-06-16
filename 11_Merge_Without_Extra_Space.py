992a9800942544ffedd245a3db5c6510d118fa9d
Sudhanshu Chaudhary <73557822+sudh29@users.noreply.github.com>
2025-06-16 13:21:38 +0530

    Update 11_Merge_Without_Extra_Space.py

--- 11_Merge_Without_Extra_Space.py
+++ 11_Merge_Without_Extra_Space.py
@@ -13,3 +13,38 @@ class Solution:
                 k-=1
         arr1.sort()
         arr2.sort()
+
+        # nums = []
+        # i,j=0,0
+        # while i<m and j<n:
+        #     if nums1[i]<=nums2[j]:
+        #         nums.append(nums1[i])
+        #         i+=1
+        #     else:
+        #         nums.append(nums2[j])
+        #         j+=1
+        # while i<m:
+        #     nums.append(nums1[i])
+        #     i+=1
+        # while j<n:
+        #     nums.append(nums2[j])
+        #     j+=1
+        # for i in range(len(nums)):
+        #     nums1[i] = nums[i]
+        # return nums1
+
+        # In place without extra space
+        # i,j,k=m-1,n-1,m+n-1
+        # while i>=0 and j>=0:
+        #     if nums1[i]>nums2[j]:
+        #         nums1[k]=nums1[i]
+        #         i-=1
+        #     else:
+        #         nums1[k]=nums2[j]
+        #         j-=1
+        #     k-=1
+        # while j>=0:
+        #     nums1[k]=nums2[j]
+        #     j-=1
+        #     k-=1
+        # return nums1
